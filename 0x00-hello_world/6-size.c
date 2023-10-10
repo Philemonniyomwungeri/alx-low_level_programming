7bc13f9c22e930cac48961b6306a093b84fd5f1f
Philemon Niyomwungeri <papy37788@gmail.com>
2023-10-09 17:21:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  * main - A program that prints the size of various computer types
+ *This program prints the size of various computer types
+ * (char, int, long int, long long int, float) on the computer it is 
+ * compiled and run on. It demonstrates the usage of the sizeof operator 
+ * to determine the size of different data types in bytes.
  * Return 0 (Success)
  */
-int main (void)
+int main(void)
 {
 char a;
 int b;
@@ -11,11 +16,10 @@ long int c;
 lon long int d;
 float f;
 
-printf("Size of a char: %lu byte(s)\n",(unisigned long)sizeof(0));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("Size of a long int:%lu byte(s)\n",(unsigned long )sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n",(unsigned)sizeof(d));
-printf ("Size of a float: %lu byte(s)\n",(unsigned long sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unisigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long sizeof(f));
 return (0);
 }
-
