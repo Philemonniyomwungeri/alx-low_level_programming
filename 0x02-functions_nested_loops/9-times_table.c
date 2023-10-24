c457644e6d5c6e605f37cf8af71da34f192dabc4
Philemon Niyomwungeri <papy37788@gmail.com>
2023-10-24 05:06:48 -0700

    9-mnbj

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+    int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar('0');
+        _putchar(',');
+        _putchar(' ');
+
+        for (j = 1; j <= 9; j++)
+        {
+            result = i * j;
+
+            if (result < 10)
+            {
+                _putchar(' ');
+            }
+            else
+            {
+                _putchar((result / 10) + '0');
+            }
+
+            _putchar((result % 10) + '0');
+
+            if (j < 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+
+        _putchar('\n');
+    }
+}
+
