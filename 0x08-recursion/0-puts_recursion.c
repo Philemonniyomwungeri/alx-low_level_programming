2b66fcaaa92296c15f89be7ced5f6e549c543698
Philemon Niyomwungeri <papy37788@gmail.com>
2023-10-26 08:43:48 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line using recursion
- * @s: The input string to be printed
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The input string.
  */
 void _puts_recursion(char *s)
 {
     if (*s == '\0')
     {
-        putchar('\n'); // assuming you have _putchar function implemented, or you can use putchar from stdio.h
+        _putchar('\n');
         return;
     }
-
-    putchar(*s); // assuming you have _putchar function implemented, or you can use putchar from stdio.h
+    _putchar(*s);
     _puts_recursion(s + 1);
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putcharchar(char);
 void _puts_recursion(char *s);
-void _print_rev_recursion(*s);
-int _strlen_recursion(*s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int -sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
