7e075b909df9b3a8655cb4739ad5ccfc3bd517bc
Philemon Niyomwungeri <papy37788@gmail.com>
2023-10-27 04:28:49 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: The input string.
+ * _print_rev_recursion - Prints a string in reverse using recursion
+ * @s: The string to be printed in reverse
+ *
+ * Description: This function prints a string in reverse by calling
+ * itself recursively until it reaches the end of the string, then
+ * prints the characters in reverse order.
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,21 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to print
- *
- * Return: On success 1, on error -1.
- */
 int _putchar(char c);
+void _print_rev_recursion(char *s);
 
-/**
- * other_function - Description of what the function does
- * @param: Description of the parameter if applicable
- *
- * Return: Description of what the function returns if applicable
- */
-/* Add more function prototypes as needed */
+/* Add other function prototypes as needed */
 
 #endif /* MAIN_H */
 
