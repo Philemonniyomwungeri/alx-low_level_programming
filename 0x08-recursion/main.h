b7b243306d8e35db1df1d9ea60e47d62e1883ffe
Philemon Niyomwungeri <papy37788@gmail.com>
2023-10-27 04:12:26 -0700

    0-puts_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int wildcmp(char *s1, char *s2);
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c);
+
+/**
+ * other_function - Description of what the function does
+ * @param: Description of the parameter if applicable
+ *
+ * Return: Description of what the function returns if applicable
+ */
+/* Add more function prototypes as needed */
 
 #endif /* MAIN_H */
 
