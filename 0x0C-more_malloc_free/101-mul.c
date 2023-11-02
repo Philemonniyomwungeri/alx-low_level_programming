2e6b0121ef02993bd78ac8df169a09db4db8ed91
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-02 15:57:28 -0700

    bvbvbvbv

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,99 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * multiply - Multiplies two positive numbers represented as strings
- * @num1: First number as string
- * @num2: Second number as string
- * Return: Pointer to the result of multiplication as a string
- */
-char *multiply(char *num1, char *num2)
+int	_isdigit(char *str)
 {
-    int len1 = 0, len2 = 0, i, j, carry = 0;
-    char *result;
-
-    while (num1[len1] != '\0')
-        len1++;
-    while (num2[len2] != '\0')
-        len2++;
-
-    result = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (result == NULL)
-    {
-        fprintf(stderr, "Error: Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (i = 0; i < len1 + len2; i++)
-        result[i] = '0';
-    result[i] = '\0';
-
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        int n1 = num1[i] - '0';
-        carry = 0;
-
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            int n2 = num2[j] - '0';
-            int sum = (n1 * n2) + (result[i + j + 1] - '0') + carry;
-            carry = sum / 10;
-            result[i + j + 1] = (sum % 10) + '0';
-        }
-
-        result[i + j + 1] += carry;
-    }
-
-    int start = 0;
-    while (result[start] == '0')
-        start++;
-
-    for (i = 0; i < len1 + len2 - start + 1; i++)
-        result[i] = result[i + start];
-    result[i] = '\0';
-
-    return result;
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
 }
 
-/**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array containing the command-line arguments
- * Return: 0 if successful, 1 if incorrect number of arguments, 98 if invalid input
- */
-int main(int argc, char *argv[])
+int	_strlen(char *str)
 {
-    if (argc != 3)
-    {
-        fprintf(stderr, "Error: Incorrect number of arguments\n");
-        return (EXIT_FAILURE);
-    }
+	int len = 0;
 
-    char *num1 = argv[1];
-    char *num2 = argv[2];
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
 
-    for (int i = 0; num1[i] != '\0'; i++)
-    {
-        if (num1[i] < '0' || num1[i] > '9')
-        {
-            fprintf(stderr, "Error: Invalid input, num1\n");
-            return (EXIT_FAILURE);
-        }
-    }
+void	multiply(char *num1, char *num2)
+{
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
+	int *result = calloc(len1 + len2, sizeof(int));
 
-    for (int i = 0; num2[i] != '\0'; i++)
-    {
-        if (num2[i] < '0' || num2[i] > '9')
-        {
-            fprintf(stderr, "Error: Invalid input, num2\n");
-            return (EXIT_FAILURE);
-        }
-    }
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    char *result = multiply(num1, num2);
-    printf("%s\n", result);
-    free(result);
+	for (int i = len1 - 1; i >= 0; i--)
+	{
+		for (int j = len2 - 1; j >= 0; j--)
+		{
+			int mul = (num1[i] - '0') * (num2[j] - '0');
+			int sum = mul + result[i + j + 1];
+			result[i + j + 1] = sum % 10;
+			result[i + j] += sum / 10;
+		}
+	}
 
-    return (EXIT_SUCCESS);
+	int non_zero_found = 0;
+	for (int i = 0; i < len1 + len2; i++)
+	{
+		if (result[i] != 0 || non_zero_found)
+		{
+			non_zero_found = 1;
+			printf("%d", result[i]);
+		}
+	}
+
+	if (!non_zero_found)
+	{
+		printf("0");
+	}
+	printf("\n");
+
+	free(result);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	multiply(argv[1], argv[2]);
+	return (0);
 }
 
