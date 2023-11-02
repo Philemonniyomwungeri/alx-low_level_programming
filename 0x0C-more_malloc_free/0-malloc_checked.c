890463dd4ce55e6bc5d6084b0a251b2e09441c15
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-02 08:34:51 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,20 +4,21 @@
 
 /**
  * malloc_checked - Allocates memory using malloc.
- * @b: Size of the memory to allocate.
+ * @b: Size of the memory block to be allocated.
  *
  * Return: A pointer to the allocated memory.
- *         If malloc fails, the function terminates the process with status 98.
+ *         If malloc fails, the malloc_checked function should cause
+ *         normal process termination with a status value of 98.
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
+
 	if (ptr == NULL)
-	{
 		exit(98);
-	}
+
 	return (ptr);
 }
 
