3ac17b03048438f1ac13c1d349b0ee6f01ec79bf
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-03 05:30:39 -0700

    sdfss

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,11 +3,10 @@
 /**
  * _putchar - Writes a character to the standard output (stdout).
  * @c: The character to be written.
- * Return: On success, the number of characters written is returned.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success, 1 is returned. On error, -1 is returned.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
 
--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
-int main() {
-    printf("At the end of the day, my goal was to be the best hacker\n");
-    return 0;
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
 
