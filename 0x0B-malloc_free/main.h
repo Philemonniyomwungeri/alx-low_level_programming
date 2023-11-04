3725065d7a6884556465a553ff14062e9e69adbc
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-04 14:45:07 -0700

    g

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-
-/* Function prototypes */
-char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
 
