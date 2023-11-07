b2ba39433d696246cbb5643b5037888cabfddd3e
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-06 16:04:30 -0800

    vcbnxmz,

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -31,5 +31,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /* Function prototype for creating a new dog */
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
