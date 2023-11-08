80f8db0b71f9b8c89be358c2cb949705da4a219f
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-08 10:17:31 -0800

    c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,39 +2,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* Function implementations */
+/**
+ * op_add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - Divides two integers.
+ * @a: The first integer.
+ * @b: The second integer (non-zero).
+ *
+ * Return: The result of integer division of a by b.
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
+/**
+ * op_mod - Computes the remainder of the division of two integers.
+ * @a: The first integer.
+ * @b: The second integer (non-zero).
+ *
+ * Return: The remainder of the division of a by b.
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
 
