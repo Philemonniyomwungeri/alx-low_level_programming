dd7a5629a12bb9ddc1f973773b0c9fa7083743b1
Philemon Niyomwungeri <papy37788@gmail.com>
2023-11-24 13:43:55 -0800

    crackme3

--- 0x14-bit_manipulation/101-main.c
+++ 0x14-bit_manipulation/101-main.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
+/**
+ * password - Function to get the password.
+ *
+ * Return: The password.
+ */
+unsigned long int password(void)
+{
+	return 101; // Replace with your actual password
+}
 
 /**
  * main - Entry point of the program.
  *
- * @argc: Number of command line arguments.
- * @argv: Array containing command line arguments.
- *
- * Return: Always 0 (Success).
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	if (argc != 2)
+	unsigned long int password_value;
+
+	password_value = password();  // Call the password function to get the password
+
+	FILE *file = fopen("101-password", "w");  // Open the file for writing
+	if (file == NULL)
 	{
-		fprintf(stderr, "Usage: %s <password>\n", argv[0]);
-		return (EXIT_FAILURE);
+		fprintf(stderr, "Error opening file.\n");
+		return (1);
 	}
 
-	char *password = argv[1];
-	char command[100];
+	fprintf(file, "%lu", password_value);  // Write the password to the file
+	fclose(file);  // Close the file
 
-	snprintf(command, sizeof(command), "./crackme3 %s", password);
-
-	int result = system(command);
-
-	if (WIFEXITED(result) && WEXITSTATUS(result) == 0)
-	{
-		printf("Congratulations!\n");
-	}
-	else
-	{
-		printf("Try again!\n");
-	}
-
-	return (EXIT_SUCCESS);
+	return (0);
 }
 
